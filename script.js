20abc4eaee058a1c4547362de90fa5bb12217d0b
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2021-09-01 10:24:12 +0900

    🎉 変換できるように

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 const complement = () => {
   const input = document.getElementById("input-form").value
-  document.getElementById("output-form").value = input
+  document.getElementById("output-form").value = complementText(input)
 }
+
+/**
+ * `@xxxx` を [@xxxx](https://trap.jp/author/xxxx/) に変える
+ * @param {string} text
+ * @return {string}
+ */
+const complementText = (text) => {
+  const getUserName = /(?<!\[)@([a-zA-Z0-9](?:[a-zA-Z0-9_]*[a-zA-Z0-9])?)/g
+  return text.replace(getUserName, '[@$1](https://trap.jp/author/$1/)')
+}
+
